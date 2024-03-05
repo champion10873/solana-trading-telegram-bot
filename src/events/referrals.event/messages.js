eceb947aca43a9242259ef348084f7521d876573
devpepesui <contact@pepesui.ceo>
2024-03-05 15:11:09 -0300

    Fixed ref msg

--- src/events/referrals.event/messages.js
+++ src/events/referrals.event/messages.js
@@ -4,12 +4,12 @@ const LAMPORTS_PER_SOL = 1_000_000_000;
 const referralsMsg = ({ code, referrals, income }) => `
   <b>REFERRALS:</b>
 
-  🔗 Your reflink: <pre>https://t.me/tonksniper_bot?start=ref_${code}</pre>
+  🔗 Your reflink: <a href="https://t.me/tonksniper_bot?start=ref_${code}">https://t.me/tonksniper_bot?start=ref_${code}</a>
 
   👥 Referrals: <b>${referrals}</b>
   💰 Lifetime SOL earned: <b>${income / LAMPORTS_PER_SOL} SOL</b>
 
-  <i>🔥 Rewards are updated at least every 24 hours and rewards are automatically deposited to your Tonk Sniper balance.
+  <i>🔥 Rewards are updated at real time and rewards are automatically deposited to your Tonk Sniper balance.
 
   Refer your friends and earn <b>30</b>% of their fees!</i>
 `;
