113aeab5b70400354a2d55fb9d9ed7369cfec22a
champion10873 <erwinhofmann923@gmail.com>
2025-03-13 21:39:30 +0000

    [update]: Replace reflink and contact info

--- src/constants/descriptions.js
+++ src/constants/descriptions.js
@@ -1,11 +1,13 @@
 const { trim } = require('@/utils');
 
 const description = () => `
-🐶 Welcome to Tonk Sniper
+🐶 Welcome to Tonk Sniper(Solana Version)
 
 🚀 Solana’s fastest bot to trade any coin (SPL token), and Tonk Sniper official Telegram Sniper Bot.
 
 🤌 Blazingly-fast trading at your fingertips. Use /start to open the main menu and start using all our features - fast swaps, new token alerts, trade tracking and PNL.
+
+Developer: @champion923
 `;
 
 const shortDescription = () => `
--- src/events/referrals.event/messages.js
+++ src/events/referrals.event/messages.js
@@ -4,7 +4,7 @@ const LAMPORTS_PER_SOL = 1_000_000_000;
 const referralsMsg = ({ code, referrals, income }) => `
   <b>REFERRALS:</b>
 
-  🔗 Your reflink: <a href="https://t.me/tonksniper_bot?start=ref_${code}">https://t.me/tonksniper_bot?start=ref_${code}</a>
+  🔗 Your reflink: <a href="https://t.me/${process.env.BOT_USERNAME}?start=ref_${code}">https://t.me/${process.env.BOT_USERNAME}?start=ref_${code}</a>
 
   👥 Referrals: <b>${referrals}</b>
   💰 Lifetime SOL earned: <b>${income / LAMPORTS_PER_SOL} SOL</b>
